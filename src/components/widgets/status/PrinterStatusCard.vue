60d6ddd7e5d00ae5db32c28690d67bf1922d5680
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2026-09-22 21:30:31 -0400

    Fix UI print start and cleanup dead MMU code

--- src/components/widgets/status/PrinterStatusCard.vue
+++ src/components/widgets/status/PrinterStatusCard.vue
@@ -81,7 +81,7 @@ import StatusControls from './StatusControls.vue'
 import StatusTab from './StatusTab.vue'
 import ReprintTab from './ReprintTab.vue'
 import type { TimeEstimates } from '@/store/printer/types'
-import getFilePaths from '@/util/get-file-paths'
+
 import CFSMatchDialog from '@/components/cfs/CFSMatchDialog.vue'
 
 @Component({
@@ -140,29 +140,13 @@ export default class PrinterStatusCard extends Mixins(StateMixin) {
   }
 
   handlePrint (filename: string) {
-    // Always intercept print to show CFS mapping dialog
-    this.cfsMatchDialogState = {
-      open: true,
-      filename
-    }
-    return
-
     if (this.$typedState.printer.printer.mmu?.enabled === true) {
-      const { rootPath, filename: filenameOnly } = getFilePaths(filename, 'gcodes')
-      const fileWithMeta = this.$typedGetters['files/getFile'](rootPath, filenameOnly)
-
-      if (fileWithMeta != null && 'referenced_tools' in (fileWithMeta as any)) {
-        const mmuPrint = ((fileWithMeta as any).referenced_tools?.length ?? 1) > 1 || this.$typedState.printer.printer.mmu!.gate !== -2
-
-        if (mmuPrint) {
-          this.$typedCommit('mmu/setDialogState', {
-            show: true,
-            filename
-          })
-
-          return
-        }
+      // Always intercept print to show CFS mapping dialog
+      this.cfsMatchDialogState = {
+        open: true,
+        filename
       }
+      return
     }
 
     const spoolmanSupported: boolean = this.$typedGetters['spoolman/getAvailable']
